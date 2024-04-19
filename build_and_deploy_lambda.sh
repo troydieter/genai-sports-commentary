82265be6b4979d270e4d3de674f71c880634bf25
EC2 Default User <ec2-user@ip-10-16-76-33.ec2.internal>
2024-04-19 00:03:27 +0000

    fixed a few things

--- build_and_deploy_lambda.sh
+++ build_and_deploy_lambda.sh
@@ -6,5 +6,8 @@ rm -rf kinesis-stream-processor.zip packages
 pip install --target ./packages  urllib3==1.26.15 boto3
 cd packages && zip -r ../kinesis-stream-processor.zip . 
 cd .. && zip kinesis-stream-processor.zip lambda_function.py 
-aws lambda update-function-code --function-name bedrock-play-by-play-commentary-processor --zip-file fileb://kinesis-stream-processor.zip
+# aws lambda update-function-code --function-name bedrock-play-by-play-commentary-processor --zip-file fileb://kinesis-stream-processor.zip
+aws iam create-role --role-name GenAILambdaExecutionRole --assume-role-policy-document file://../trust-policy.json
+aws iam attach-role-policy --role-name GenAILambdaExecutionRole --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+aws lambda create-function --function-name bedrock-play-by-play-commentary-processor --role "arn:aws:iam::550767824695:role/GenAILambdaExecutionRole" --runtime "python3.9" --handler "lambda_function" --zip-file fileb://kinesis-stream-processor.zip
 rm -rf kinesis-stream-processor.zip
